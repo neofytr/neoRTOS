c638d1437ddc3eb95ee743abea4f6a57a656db27
neofytr <imagination12344321.com@gmail.com>
2025-02-05 17:53:21 +0530

    init thread

---
+++ kernel/includes/neo_threads.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+#define STACK_SIZE (40U) // in words (32-bits)
+
+typedef struct
+{
+    __attribute__((aligned(8))) uint32_t stack[40]; // aligned to 8 bytes so that stack_ptr is aligned to 8 bytes (following ACPSR)
+    uint8_t *stack_ptr;
+} neo_thread_t;
+
+void init_thread(neo_thread_t *thread);
---
+++ kernel/source/neo_threads.c
@@ -0,0 +1,88 @@
+#include "neo_threads.h"
+
+#define PROCESSOR_MODE_BIT (24U)
+
+void init_thread(neo_thread_t *thread, void (*thread_function)(void *arg), void *thread_function_arg)
+{
+    /*
+     * Thread Stack Frame Initialization for ARM Cortex-M4
+     * ------------------------------------------------
+     * This implementation initializes a thread's stack frame to exploit the processor's
+     * interrupt return mechanism (exception exit) for context switching. The approach
+     * creates a "fake" exception stack frame that the processor will pop during an
+     * interrupt return instruction (LR = 0xFFFFFFF9).
+     *
+     * Exception Stack Frame Layout (Bottom to Top):
+     * - R0-R3: General-purpose registers
+     * - R12: IP (Intra-Procedure scratch register)
+     * - LR: Link Register (Return address)
+     * - PC: Program Counter (Thread entry point)
+     * - xPSR: Program Status Register
+     *
+     * Important Implementation Notes:
+     * 1. FPU must be disabled as this implementation doesn't handle FPU context
+     * 2. Stack grows downward on ARM architecture
+     * 3. Stack must be 8-byte aligned as per ARM AAPCS requirements
+     * 4. Thread functions must be endless loops and never return
+     *
+     * Context Switch Mechanism:
+     * When executing the interrupt return sequence, the processor will:
+     * 1. Pop all registers from the stack frame
+     * 2. Update the execution context based on the popped values
+     * 3. Begin execution at the address specified in PC
+     */
+
+    /* thread_function is the function the thread will execute; thread_function_arg is the initial argument passed to the thread function */
+
+    /* Initialize stack pointer to end of stack area */
+    thread->stack_ptr = (uint8_t *)thread->stack + 4 * STACK_SIZE;
+
+    uint32_t *ptr = (uint32_t *)thread->stack_ptr;
+
+    /*
+     * Stack Frame Construction (in reverse order of exception stack frame)
+     * Each register is pushed onto the stack to create the initial thread context
+     */
+
+    /* xPSR - Program Status Register
+     * Set Thumb state bit (bit 24) to ensure execution in Thumb state
+     * All other bits are initialized to 0 as per reset behavior
+     */
+    *(--ptr) = 0x1U << PROCESSOR_MODE_BIT;
+
+    /* PC - Program Counter
+     * Set to thread_function address
+     * This is where execution will begin when the context is restored
+     */
+    *(--ptr) = (uintptr_t)thread_function;
+
+    /* LR - Link Register
+     * Set to 0 since thread_function should never return
+     * In a proper implementation, this could point to an error handler
+     */
+    *(--ptr) = 0x0U;
+
+    /* R12 (IP) - Intra-Procedure scratch register
+     * Initialized to 0 as it's caller-saved and not needed initially
+     */
+    *(--ptr) = 0x0U;
+
+    /* R3 - General-purpose register
+     * Initialized to 0 as no initial argument passing is implemented
+     */
+    *(--ptr) = 0x0U;
+
+    /* R2 - General-purpose register */
+    *(--ptr) = 0x0U;
+
+    /* R1 - General-purpose register */
+    *(--ptr) = 0x0U;
+
+    /* R0 - General-purpose register
+     * First argument register; contains the argument to the thread function
+     */
+    *(--ptr) = (uintptr_t)thread_function_arg;
+
+    /* Update thread's stack pointer to point to the top of our constructed frame */
+    thread->stack_ptr = (uint8_t *)ptr;
+}
