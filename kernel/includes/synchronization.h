a58cc9478d2ea40e100d8f50661a49d21184dbf5
neofytr <imagination12344321.com@gmail.com>
2025-02-07 09:50:59 +0530

    synchro

---
+++ kernel/includes/synchronization.h
@@ -0,0 +1,6 @@
+#ifndef SYNCHRONIZATION_H
+#define SYNCHRONIZATION_H
+
+/* There are synchronization instruction primitives in Cortex M4; See Section 2.2.7 of the programmer's manual*/
+
+#endif // SYNCHRONIZATION_H
